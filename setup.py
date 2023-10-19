3c5a5141aeb38c8719bda8c43b60fa7f33bcf95c
Yu Liu <yuki.liu@utexas.edu>
2023-10-19 12:53:12 -0500

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="pytest-inline",
-    version="1.0.4",
+    version="1.0.5",
     author="Yu Liu",
     author_email="yuki.liu@utexas.edu",
     maintainer="Alan Han, Yu Liu, Pengyu Nie, Zachary William Thurston",
